4ca0a0c504ded6501a8934c9a426ef95182098a6
PrinceXtremeX <princextreme429@gmail.com>
2025-07-04 22:06:22 -0400

    Update convert-tovideo.js

--- plugins/convert-tovideo.js
+++ plugins/convert-tovideo.js
@@ -6,7 +6,7 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const { spawn } = require('child_process');
 
 // Configuration
-const COVER_URL = 'https://files.catbox.moe/2rgdc4.jpg';
+const COVER_URL = 'https://files.catbox.moe/r20dpn.mp3';
 const TEMP_DIR = path.join(__dirname, '../temp');
 const MAX_RETRIES = 3;
 
