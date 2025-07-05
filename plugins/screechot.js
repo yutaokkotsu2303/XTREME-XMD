9f0b82081d3b056f5af1b3a2b5872610f73df1b0
PrinceXtremeX <princextreme429@gmail.com>
2025-07-04 22:38:50 -0400

    Create screechot.js

---
+++ plugins/screechot.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "ss2",
+  alias: ["ssweb2"],
+  react: "🚀",
+  desc: "Download screenshot of a given link.",
+  category: "convert",
+  use: ".ss <link>",
+  filename: __filename,
+}, 
+async (conn, mek, m, {
+  from, l, quoted, body, isCmd, command, args, q, isGroup, sender, 
+  senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, 
+  groupMetadata, groupName, participants, isItzcp, groupAdmins, 
+  isBotAdmins, isAdmins, reply 
+}) => {
+  if (!q) {
+    return reply("*Please provide a url to capture a screenshot.*");
+  }
+
+  try {
+    
+    const response = await axios.get(`https://bk9.fun/tools/screenshot?device=tablet&url= ${q}`);
+    const screenshotUrl = response.data.screenshotUrl;
+
+    // give credit and use
+    const imageMessage = {
+      image: { url: screenshotUrl },
+      caption: "*SS WEB*"};
+
+    await conn.sendMessage(from, imageMessage, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply("Failed to capture the screenshot. Please try again.");
+  }
+});
