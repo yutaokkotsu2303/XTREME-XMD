f940356d3e57b921b1363b241dc4a409badfdec2
PrinceXtremeX <princextreme429@gmail.com>
2025-08-15 08:52:47 -0600

    Update config.js

--- config.js
+++ config.js
@@ -80,7 +80,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "527145550855",
+DEV: process.env.DEV || "528145550855",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
